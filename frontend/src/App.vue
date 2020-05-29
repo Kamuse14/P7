303d3961c516389fa660f4c5f77d8103001511f7
Karine Grassi <karine-felicia.grassi@ac-normandie.fr>
2020-05-29 09:25:18 +0200

    installation Vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/icon.png">
+    <HelloWorld msg="Partagez avec Tweetomania !"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+};
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  width: 20em;
+}
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="header">
+		
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Header",
+		props: {
+
+		}
+	};
+</script>
+
+<style scoped>
+
+</style>
